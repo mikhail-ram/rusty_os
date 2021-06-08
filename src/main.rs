a63e4b84386c6a3f010476d9025fd83357ab7046
Mikhail <mikhailethanram@gmail.com>
2021-06-09 01:02:08 +0530

    Refactored code, making it smarter, faster and more idiomatic.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-use chrono::{DateTime, Local};
-use strum::IntoEnumIterator;
+mod logo;
+mod utility;
 use strum_macros::EnumIter;
+use strum::IntoEnumIterator;
+use std::collections::HashMap;
 
 #[derive(Debug, EnumIter)]
 enum Functions {
@@ -14,71 +16,47 @@ enum Functions {
     Startup,
     Back
 }
+
 const FUNCTIONS: [&str; 9] = ["Help", "Settings", "Web_Browser",
                               "RaptorText_Editor", "Calculator",
                               "RaptorImage_Editor", "RaptorFile_Xplorer",
                               "Startup", "Back"];
 
-fn input(string: &str) -> String {
-    let mut input = String::new();
-    if string != "" {
-        println!("{}", string);
-    }
-    std::io::stdin().read_line(&mut input).unwrap();
-    input
-}
-
-macro_rules! input {
-    ($s: expr) => {
-        input($s);
-    };
-    () => {
-        input("")
-    };
-}
-
-fn border(string: &str) {
-    let border_vertical: String = format!(" {} ", std::iter::repeat("-").take(string.len() + 2).collect::<String>());
-    println!("{}", border_vertical);
-    println!("| {} |", string);
-    println!("{}", border_vertical);
-}
-
 fn exec_loop() {
-    /*
-    for (index, function) in FUNCTIONS.iter().enumerate() {
-        println!("[{}] {}", index + 1, function);
-    }
-    */
     for (index, function) in Functions::iter().enumerate() {
-        println!("[{}] {:?}", index + 1, function);
+        let index = index + 1;
+        if index % 2 == 0 {
+            print!("[{}] {:?}\n", index, function);
+        }
+        else {
+            print!("[{}] {:?} ", index, function);
+        }
     }
-}
-
-fn is_string_numeric(string: &String) -> bool {
-    let list: Vec<bool> = string.chars().map(|c| c.is_numeric()).collect();
-    let list = &list[..list.len()-2];
-    list.iter().all(|v| *v == true)
 }
 
 fn main() {
-    const TEXT: &str = "|||||  |||||  |||||  || ||  ||||        |||||    |||||
-|| ||  || ||  || ||   |||   || ||       || ||    ||
-||||   |||||  ||||     |    || ||       || ||     ||
-|| ||  || ||  ||       |    ||||        |||||  |||||";
-
-    let now: DateTime<Local> = Local::now();
-    println!("{}", now.format("%a, %d %b %Y %H:%M."));
-    println!("{}", TEXT);
-    println!("Welcome to Rapyd_OS.");
-    println!("type in the Menu name to go to it and 'Back' to return here. If help is needed, type in 'Help()'");
+    let mut functions_hash = HashMap::new();
+    for (index, function) in Functions::iter().enumerate() {
+        functions_hash.insert(index + 1, function);
+    }
+    utility::date();
+    println!("{}", logo::TEXT);
+    println!("Welcome to Rusty_OS.");
+    println!("type in the Menu name to go to it and 'Back' to return here.");
+    println!("If help is needed, type in 'Help()'.");
     println!("-------------------");
-    border("Main Menu");
+    utility::border("Main Menu");
     exec_loop();
-    let input = input!("Pick a choice:");
-    if is_string_numeric(&input) {
-        let choice: usize = input[..input.len()-2].parse::<usize>().unwrap() - 1;
-        println!("{}", FUNCTIONS[choice])
+    let input = utility::input("\n[*] ").trim_end().to_owned();
+    // TODO: Replace if/else logic with match clause.
+    if input == "" {
+        println!("Please enter a valid number");
+    } else if input.chars().all(|c| c.is_digit(10)) {
+        let choice: usize = input.parse::<usize>().unwrap() - 1;
+        println!("{}", FUNCTIONS[choice]);
+    } else if input.chars().all(|c| c.is_ascii_alphabetic()) {
+        println!("Alphabetic!");
     } else {
+        println!("Other");
     }
 }
